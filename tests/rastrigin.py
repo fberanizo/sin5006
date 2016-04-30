4d027f6c4200d4385ca1e54d4de8cc92dc765b10
Fabio Beranizo <fabio.beranizo@gmail.com>
2016-04-30 20:28:51 -0300

    Adds another termination criteria

--- tests/rastrigin.py
+++ tests/rastrigin.py
@@ -20,7 +20,7 @@ class Rastrigin(unittest.TestCase):
         Termination criteria: number of generations = 100
 
         Parameters:
-            population_size: [10, 21, 46, 100, 215, 464, 1000, 2154, 4641, 10000]
+            population_size: [8, 19, 47, 115, 282, 689, 1680, 4096]
                 (numbers spaced evenly on a log scale)
             crossover rate, reproduction rate, mutation rate: varying from 0.0 to 1.0
                 (all possible combinations that sum to 1.0 ex: [0.2, 0.5, 0.3])
@@ -28,7 +28,7 @@ class Rastrigin(unittest.TestCase):
         print 'Starting test_case1: ONE-POINT CROSSOVER, BASIC MUTATION, ELITISM DISABLED'
 
         params = {
-            "population_size": numpy.logspace(1, 4, base=10, num=10, dtype=int),
+            "population_size": numpy.logspace(3, 12, base=2, num=8, dtype=int),
             "operators_rate": filter(lambda x: sum(x) == 1.0, itertools.product(numpy.arange(.0, 1.1, .1), repeat=3)),
             "elitism": [False],
             "termination_criteria": [ ga.NumberOfGenerationsTerminationCriteria(number_of_generations=100) ]
@@ -53,7 +53,7 @@ class Rastrigin(unittest.TestCase):
         Termination criteria: number of generations = 100
 
         Parameters:
-            population_size: [10, 21, 46, 100, 215, 464, 1000, 2154, 4641, 10000]
+            population_size: [8, 19, 47, 115, 282, 689, 1680, 4096]
                 (numbers spaced evenly on a log scale)
             crossover rate, reproduction rate, mutation rate: varying from 0.0 to 1.0
                 (all possible combinations that sum to 1.0 ex: [0.2, 0.5, 0.3])
@@ -61,7 +61,7 @@ class Rastrigin(unittest.TestCase):
         print "Starting test_case2: ONE-POINT CROSSOVER, BASIC MUTATION, ELITISM ENABLED"
 
         params = {
-            "population_size": numpy.logspace(1, 4, base=10, num=10, dtype=int),
+            "population_size": numpy.logspace(3, 12, base=2, num=8, dtype=int),
             "operators_rate": filter(lambda x: sum(x) == 1.0, itertools.product(numpy.arange(.0, 1.1, .1), repeat=3)),
             "elitism": [True],
             "termination_criteria": [ ga.NumberOfGenerationsTerminationCriteria(number_of_generations=100) ]
@@ -86,7 +86,7 @@ class Rastrigin(unittest.TestCase):
         Termination criteria: number of generations = 100
 
         Parameters:
-            population_size: [10, 21, 46, 100, 215, 464, 1000, 2154, 4641, 10000]
+            population_size: [8, 19, 47, 115, 282, 689, 1680, 4096]
                 (numbers spaced evenly on a log scale)
             crossover rate, reproduction rate, mutation rate: varying from 0.0 to 1.0
                 (all possible combinations that sum to 1.0 ex: [0.2, 0.5, 0.3])
@@ -94,7 +94,7 @@ class Rastrigin(unittest.TestCase):
         print "Starting test_case3: UNIFORM CROSSOVER, BASIC MUTATION, ELITISM ENABLED"
 
         params = {
-            "population_size": numpy.logspace(1, 4, base=10, num=10, dtype=int),
+            "population_size": numpy.logspace(3, 12, base=2, num=8, dtype=int),
             "operators_rate": filter(lambda x: sum(x) == 1.0, itertools.product(numpy.arange(.0, 1.1, .1), repeat=3)),
             "elitism": [True],
             "termination_criteria": [ ga.NumberOfGenerationsTerminationCriteria(number_of_generations=100) ]
@@ -119,7 +119,7 @@ class Rastrigin(unittest.TestCase):
         Termination criteria: number of generations = 100
 
         Parameters:
-            population_size: [10, 21, 46, 100, 215, 464, 1000, 2154, 4641, 10000]
+            population_size: [8, 19, 47, 115, 282, 689, 1680, 4096]
                 (numbers spaced evenly on a log scale)
             crossover rate, reproduction rate, mutation rate: varying from 0.0 to 1.0
                 (all possible combinations that sum to 1.0 ex: [0.2, 0.5, 0.3])
@@ -127,7 +127,7 @@ class Rastrigin(unittest.TestCase):
         print 'Starting test_case4: ONE-POINT CROSSOVER, PERMUTATION MUTATION, ELITISM ENABLED'
 
         params = {
-            "population_size": numpy.logspace(1, 4, base=10, num=10, dtype=int),
+            "population_size": numpy.logspace(3, 12, base=2, num=8, dtype=int),
             "operators_rate": filter(lambda x: sum(x) == 1.0, itertools.product(numpy.arange(.0, 1.1, .1), repeat=3)),
             "elitism": [True],
             "termination_criteria": [ ga.NumberOfGenerationsTerminationCriteria(number_of_generations=100) ]
@@ -142,6 +142,38 @@ class Rastrigin(unittest.TestCase):
         print "Finished. Results are at: /home/fabio/sin5006/tests/results/test_case4.csv"
         assert True
 
+    def test_case5(self):
+        """
+        Chromosome: float array containing x and y values
+        Selection: roulette-wheel
+        Crossover operator: one-point crossover
+        Mutation operator: basic (replaces x or y by another valid value)
+        Elitism is enabled
+        Termination criteria: execution_time = 2s
+
+        Parameters:
+            population_size: [8, 19, 47, 115, 282, 689, 1680, 4096]
+                (numbers spaced evenly on a log scale)
+            crossover rate, reproduction rate, mutation rate: varying from 0.0 to 1.0
+                (all possible combinations that sum to 1.0 ex: [0.2, 0.5, 0.3])
+        """
+        print "Starting test_case5: ONE-POINT CROSSOVER, BASIC MUTATION, ELITISM ENABLED, 2 SECONDS OF EXECUTION TIME"
+
+        params = {
+            "population_size": numpy.logspace(3, 12, base=2, num=8, dtype=int),
+            "operators_rate": filter(lambda x: sum(x) == 1.0, itertools.product(numpy.arange(.0, 1.1, .1), repeat=3)),
+            "elitism": [True],
+            "termination_criteria": [ ga.ExecutionTimeTerminationCriteria(time_in_seconds=2.0) ]
+        }
+        solver = ga.GeneticAlgorithm(optimization.RastriginIndividualFactory(crossover_method='one_point', mutation_method='basic_mutation'))
+        grid = grid_search.GridSearch(solver, params)
+        grid.search(0.0)
+        grid_scores = grid.get_grid_scores()
+
+        utils.save_scores('/home/fabio/sin5006/tests/results/test_case5.csv', grid_scores)
+
+        print "Finished. Results are at: /home/fabio/sin5006/tests/results/test_case5.csv"
+        assert True
 
 if __name__ == '__main__':
     unittest.main()
