96f7980b5ff61bd4af7852c9d733521edde540eb
Fabio Lopes <fabiol@cpqd.com.br>
2016-06-16 10:20:11 -0300

    Final version

--- tests/cvrp/correction_operator.py
+++ tests/cvrp/correction_operator.py
@@ -7,7 +7,7 @@ import unittest, ga, cvrp, grid_search, re, math, numpy, itertools, matplotlib.p
 
 class CorrectionOperator(unittest.TestCase):
     """Test cases for CVRP problem."""
-    grid_search = True
+    grid_search = False
 
     def test_1(self):
         """
